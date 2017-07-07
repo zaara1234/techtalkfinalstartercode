5923e3c01e8e680b8bc65b95c5e5d875213b7276
zaara afida <zafida.student@hsfi.us>
2017-07-07 19:20:05 +0000

    I did some changes

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // JavaScript File
 // JavaScript File
 $( document ).ready(function() {
-
-
+$('#gogo').click(function(){
+    var submit= $(".Maluma").val();
+    if(submit==="Maluma"){
+        alert("You got this!");
+    }else{
+        alert("Try Again!!");
+    }
 });
+});
+
